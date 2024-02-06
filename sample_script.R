afe1ecf258d35dd4b0559a4546ae10504f7243a0
Guillaume Patoine <guillaume.patoine@idiv.de>
2024-02-06 02:24:20 +0100

    up

--- sample_script.R
+++ sample_script.R
@@ -16,12 +16,24 @@ f("a")
 traceback()
 
 
+library(dplyr)
+iris %>% filter(Sepal.Lengt > 4)
+
+traceback()
+
+rlang::last_trace()
+rlang::last_error()
+
+
+
 # recover ----------------------------------
 
-options(error = recover) 
+options(error = recover)
+f(12)
 f("a")
 
 options(error = NULL)
+
 
 
 # Browser and breakpoints ------------------
@@ -29,7 +41,7 @@ dput(x=sample(1:30, 24))
 dput(1:5)
 dput(c(1:5, 6))
 
-source("code/example_breakpoint.r")
+source("code/example_breakpoint.R")
 dput_range(sample(1:30, 25))
 dput_range(c(1:5, 6))
 
@@ -37,6 +49,7 @@ dput_range(c(1:5, 6))
 # then with breakpoints
 
 dput_range(sample(1:30, 15))
+
 
 
 # debug() ---------------------------------
@@ -53,8 +66,15 @@ myfun(6)
 debug(myfun)
 myfun(4)
 undebug(myfun)
+
 debugonce(myfun)
 myfun(2)
 
 
 
+# Now pick a function to look into, or try to resolve an error that you had.
+# Examples:
+
+sample
+lm
+ggplot
